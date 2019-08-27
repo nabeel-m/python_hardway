767732a2ce45c922796cc1cc4b3f766feaa0b356
nabeel-m <32379996+nabeel-m@users.noreply.github.com>
2019-08-27 20:38:04 +0530

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,7 @@
+print("hello world")
+print("hello again");
+print("I like typing this.");
+print("This is fun");
+print("yay ! printing");
+print("I 'd much rather not");
+print('I "said" do not rather touch this');
---
+++ ex3.py
@@ -0,0 +1,15 @@
+my_name='Nabeel.m'
+my_age=22
+my_height=6
+my_weight=78
+my_eyes='Black'
+my_teeth='white'
+my_hair='Black'
+
+print("My name is ",my_name,".")
+print("I'm ",my_age, "years old.")
+print("My height is ",my_height," feet.")
+print("My weight is ",my_weight,"kilogram.")
+print("My eyes colour is ",my_eyes,".")
+print("My teeth colour is ",my_teeth,".")
+print("my hair colour is",my_hair,".")
