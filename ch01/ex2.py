910254f49c4dbdadf9b66917b48ce31836cf9873
Nabeel <nabeelmanf@gmail.com>
2019-08-28 06:52:27 +0530

    fix ex2.py

--- ch01/ex2.py
+++ ch01/ex2.py
@@ -7,7 +7,7 @@ cars_driven=drivers
 carpool_capacity=cars_driven * space_in_car
 average_passengers_per_car=passengers / cars_driven
 
-print("there are",cars,"available");
+print("there are",cars,"available")
 print("there are only ",drivers,"available")
 print("there will be ",car_not_driven,"empty cars today")
 print("we can transport ",carpool_capacity,"people")
