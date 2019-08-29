a1f297920cd0a080fe6c3127f4fee9d6a02550bd
Nabeel <nabeelmanf@gmail.com>
2019-08-29 11:16:51 +0530

    f ,format function

---
+++ ch01/ex5.py
@@ -0,0 +1,24 @@
+types_of_people = 10
+x = f"There are {types_of_people}types of people"
+
+binary = "binary"
+do_not = "don't"
+
+y = f"Those know {binary} and those who {do_not}"
+
+print(x)
+print(y)
+
+print(f"I said : {x}")
+print(f"I also said : {y}")
+
+hilarious = False
+
+joke_evaluation="Is that joke so funny ?! {}"
+
+print(joke_evaluation.format(hilarious))
+
+w ="Left side of a string...."
+e ="a string with right side"
+
+print(w + e)
