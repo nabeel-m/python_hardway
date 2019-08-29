d0fcd9ac346a89e5a6a560ac68de99deb1ee3b39
Nabeel <nabeelmanf@gmail.com>
2019-08-29 11:23:08 +0530

    triple quotes

---
+++ ch01/ex8.py
@@ -0,0 +1,12 @@
+days ="Sun Mon Tue Wed Thur Fri Sat"
+months = "\njan \nfeb \nmarch \napril \nmay \njune \njuly \naug"
+
+print("Here are the days:",days)
+print("Here are the months:",months)
+
+print("""
+      There's something going here
+      with three double quotes
+      we will able to type as much as we like
+      even 4 lines we want , or 5 or 6  
+      """)
