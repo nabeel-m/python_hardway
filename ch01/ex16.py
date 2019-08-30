3fdab532f7cd8a99ebb018382d9b76f9670414f9
Nabeel <nabeelmanf@gmail.com>
2019-08-30 19:46:47 +0530

    copy

--- ch01/ex16.py
+++ ch01/ex16.py
@@ -7,11 +7,12 @@ print(f"copying from {from_file} to {to_file}")
 in_file = open(from_file)
 in_data = in_file.read()
 
+
 print(f"The input file is {len(in_data)} bytes long")
 print(f"Does output file exist?{exists(to_file)}")
 print("Ready,hit RETURN to continue,CTRL-C to abort")
 input()
-
+print("\n")
 out_file = open(to_file,'w')
 out_file.write(in_data)
 
