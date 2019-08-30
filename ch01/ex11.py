aa5969af664344eb6220bee4e91ffc13b66850d1
Nabeel <nabeelmanf@gmail.com>
2019-08-30 19:28:15 +0530

    input

---
+++ ch01/ex11.py
@@ -0,0 +1,8 @@
+age = input("How old are you?:")
+height = input("How tall are you?:")
+weight = input("How moch  do you weigh?:")
+
+print(f"""
+        *  So your age is {age} 
+        *  your heigt is {height} 
+        *  your weight is{weight}""")
