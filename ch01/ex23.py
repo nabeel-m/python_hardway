235cad502be708daf87c88063cb9c8e7cc9c1745
nabeel <nabeel@localhost.localdomain>
2019-09-06 10:44:00 +0530

    else if

---
+++ ch01/ex23.py
@@ -0,0 +1,30 @@
+people = 30
+cars = 40
+trucks = 15
+
+if cars > people:
+    print("we should take the cars.")
+
+elif cars < peole:
+    print("we shouldn't take the cars")
+
+else:
+    print("we can't decide")
+
+
+if trucks > cars:
+    print("that's too many trucks")
+
+elif trucks < cars:
+    print("may be we could take the cars")    
+
+else:
+    print("we still can't decide .")
+
+if people > trucks:
+    print("Alright let's take the trucks ")
+
+else:
+    print("Fine let's stay home then")                 
+
+
