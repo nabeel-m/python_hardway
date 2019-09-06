711c3480fc615e38aca9e7ada7aa19c4278f22ee
nabeel <nabeel@localhost.localdomain>
2019-09-06 10:42:07 +0530

    if command

---
+++ ch01/ex22.py
@@ -0,0 +1,27 @@
+people = 20
+cats = 30
+dogs = 15
+
+if people < cats:
+    print("Too many cats world is doomed.")
+
+if people > cats:
+    print("Not many class world is saved")
+
+if people < dogs:
+    print("The world is dooled")
+
+if people > dogs:
+    print("The world is dry") 
+
+dogs+=5
+
+if people >= dogs:
+    print("people greater than or equal to dogs")
+
+if people <= dogs:
+    print("people less than or equal to dogs")   
+
+if people == dogs:
+    print("people are dogs")      
+
