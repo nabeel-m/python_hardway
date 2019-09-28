f0e633e95721a2a2578befb98857ee673a69d0e5
nabeel-m <32379996+nabeel-m@users.noreply.github.com>
2019-09-28 08:14:07 +0530

    Delete test_flames.py

--- ch01/test_flames.py
+++
@@ -1,8 +0,0 @@
-import unittest
-import flames1
-from flames1 import flame
-
-class Flames(unittest.TestCase):
-
-    def test_flames(self):
-        self.assertEqual(flame("sunil","sweety"),"enemy")
