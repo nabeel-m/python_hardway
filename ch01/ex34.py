2a276aa6cb57e0df3402bb29002afebfc7030a65
nabeel <nabeel@localhost.localdomain>
2019-09-28 07:56:51 +0530

    class inheritace implicit override altered

--- ch01/ex34.py
+++ ch01/ex34.py
@@ -17,7 +17,7 @@ class Child(Parent):
     def altered(self):
         print("CHILD BEFORE PARENT altered()")
         super(Child,self).altered()
-        print("CHILD AFTER PARENT altered()")
+        print("CHILD AFTER PARENT altered() ")
 
 dad = Parent()
 son = Child()
