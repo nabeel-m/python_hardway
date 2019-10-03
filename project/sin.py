159cc5380d399d1751b77e2d54f817b9c1e15835
nabeel <nabeel@localhost.localdomain>
2019-10-03 09:24:41 +0530

    finds sine values

---
+++ project/sin.py
@@ -0,0 +1,31 @@
+import math
+def factorial(a):
+        fact=1
+        for i in range(1,a+1):
+                fact=fact*i
+        return fact
+
+def sinval(x,n):
+        sin=0
+        for i in range(0,n):
+                sign=(-1)**i
+                pi=22/7
+                radiant=x*(pi/180)
+                sin=sin+((radiant**(2*i+1))/factorial(2*i+1))*sign
+                sine=sin
+        return sine
+
+def sinangle(x):
+        rad=math.radians(x)
+        sine=math.sin(rad)
+        return sine
+
+
+#x=int(input("Enter the values in degree."))
+n=int(input("Enter the no.of terms:"))
+for x in range(0,91):
+        print(f"sin{x}degree :",sinval(x,n))  
+
+x=int(input("Enter an angle:")) 
+print(f"sin{x}",sinangle(x))                    
+        
