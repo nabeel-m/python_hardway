00a9e6e56820203656d3eccc98117d64897a7925
nabeel <nabeel@localhost.localdomain>
2019-10-03 09:25:57 +0530

    unittest of sine program

---
+++ project/test_sin.py
@@ -0,0 +1,14 @@
+import unittest
+import math
+from sin import factorial
+from sin import sinval
+from sin import sinangle
+
+class SineVal(unittest.TestCase):
+    def test_factorial(self):
+        self.assertEqual(factorial(4),24)
+
+    def test_sinval(self):
+        for x in range (0,91):
+            self.assertLessEqual(sinval(x,20)-sinangle(x),0.001)
+            #self.assertEqual(sinval(45,20),sin(45))        
