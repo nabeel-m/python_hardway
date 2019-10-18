1fb601f0d4ea733a1573477a616d84b817e50d20
Sunil Thaha <3005132+sthaha@users.noreply.github.com>
2019-10-18 21:49:15 +1000

    fix sin

--- project/sin.py
+++ project/sin.py
@@ -1,31 +1,31 @@
 import math
+
 def factorial(a):
-        fact=1
-        for i in range(1,a+1):
-                fact=fact*i
-        return fact
+    fact = 1
+    for i in range(1, a+1):
+        fact *= i
+    return fact
 
-def sinval(x,n):
-        sin=0
-        for i in range(0,n):
-                sign=(-1)**i
-                pi=22/7
-                radiant=x*(pi/180)
-                sin=sin+((radiant**(2*i+1))/factorial(2*i+1))*sign
-                sine=sin
-        return sine
+DEGREE_TO_RADIANS = math.pi / 180
 
-def sinangle(x):
-        rad=math.radians(x)
-        sine=math.sin(rad)
-        return sine
+def sin_angle(angle, n):
+    radians = angle * DEGREE_TO_RADIANS
+
+    sin = 0
+    for i in range(0, n):
+        sign = (-1)**i
+        exp = 2 * i + 1
+        sin += radians ** exp / factorial(exp) * sign
+    return sin
 
 
-#x=int(input("Enter the values in degree."))
-n=int(input("Enter the no.of terms:"))
-for x in range(0,91):
-        print(f"sin{x}degree :",sinval(x,n))  
+def main():
+    n = int(input("No of terms: "))
+    angle = int(input("Angle in degrees: "))
+    print(f"sin({angle}°)", sin_angle(angle, n))
+    return 0
 
-x=int(input("Enter an angle:")) 
-print(f"sin{x}",sinangle(x))                    
-        
+
+if __name__ == '__main__':
+    main()
+
--- project/test_sin.py
+++ project/test_sin.py
@@ -1,14 +1,19 @@
 import unittest
 import math
-from sin import factorial
-from sin import sinval
-from sin import sinangle
+from sin import factorial, sin_angle
+
+def math_sin(x):
+    rad = math.radians(x)
+    return math.sin(rad)
 
 class SineVal(unittest.TestCase):
     def test_factorial(self):
-        self.assertEqual(factorial(4),24)
+        self.assertEqual(factorial(4), 24)
+        self.assertEqual(factorial(5), 120)
+        self.assertEqual(factorial(6), 720)
 
     def test_sinval(self):
-        for x in range (0,91):
-            self.assertLessEqual(sinval(x,20)-sinangle(x),0.001)
-            #self.assertEqual(sinval(45,20),sin(45))        
+        for x in range (0, 91):
+            actual = sin_angle(x, 5)
+            expected = math_sin(x)
+            self.assertLessEqual(expected - actual, 0.001)
