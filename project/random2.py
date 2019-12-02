2e43f3ab3c43b8aa6f240b7c4515f9684ecb0bf5
nabeel <nabeel@localhost.localdomain>
2019-12-02 11:02:08 +0530

    filtering the inputs

--- project/random2.py
+++ project/random2.py
@@ -9,6 +9,8 @@ class Filter:
             if self.items[i] <= self.threshold: 
                 return self.items.remove(self.items[i])  
 
+
+            
     #def display(self, size, threshold):
      #   print(self.items[len(self.items)-self.size:]
 
@@ -17,25 +19,15 @@ def main():
     t = int(input("Threshold:"))
     
     f = Filter(s,t)
-    f.items = [20, 30, 40, 50, 70, 100]
-    print(f.items)
+    f.items = [25, 10, 30, 37, 50, 11]
     
+    print(f.items)
     print("\n")
     
-    if (s <= len(f.items)):
-        for i in range(len(f.items)):
-            f.filter(t)
-        print(f.items[len(f.items)-s:])
-    else:
-        for i in range(len(f.items)):
-            f.filter(t)
-        print(f.items[:])
+    for i in range(len(f.items)):
+        f.filter(t)
+    #f.items.sort()
+    print(f.items[len(f.items)-s:])
 
-            
-
-    print("\n") 
-    
-
- 
 if __name__ == "__main__":
     main()
